acc293682db0c7da9bdd834a9f05018b9c64e9ea
RGmetab <57664349+RGmetab@users.noreply.github.com>
2023-01-04 12:09:29 +0000

    first tests

--- src/keggpull/main.py
+++ src/keggpull/main.py
@@ -1,100 +1,135 @@
+import asyncio
+import logging
 import re
 import sys
 import xml.etree.ElementTree as ET
-from time import perf_counter
 
 import requests
 
-
-def getAPI(APIin, retType):
-    # Make API requests and return the APi object
-    # Take three letter organism code and a request type:
-    # list: returns all the available pathways of the organism
-    # pathKGML: returns the KGML file of the pathway
-    # path: returns the text information of the pathway
-    if retType == "list":
-        APIreq = "http://rest.kegg.jp/list/pathway/" + APIin
-    elif retType == "pathKGML":
-        APIreq = "http://rest.kegg.jp/get/" + APIin + "/kgml"
-    elif retType == "path":
-        APIreq = "http://rest.kegg.jp/get/" + APIin
-    else:
-        sys.exit("Invalid request type")
-    APIresp = requests.get(APIreq)
-    return APIresp
+# from time import perf_counter
 
 
-def checkAPI(APIresp):
-    # Takes an API response and checks if the request was succesfull.
-    # Returns a message on success/fail
-    if APIresp.status_code == 200:
-        print("API Request Successful!")
-    else:
-        sys.exit("Request Failed!")
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.StreamHandler(sys.stdout))
 
 
-def buildPathKGML(root):
-    # Builds a list of unique (no repeats) metabolites for a given parsed (via Elementree XML parser) KGML pathway file
-    # Returns a list
-    tmpPath = []
-    tmpPathRet = []
+def pathway_list_url(user_input: str) -> str:
+    """Returns the URL for the generating list of pathways"""
+    return f"https://rest.kegg.jp/list/pathway/{user_input}"
+
+
+def pathway_kgml_url(user_input: str) -> str:
+    """Returns the URL for pathway information in kgml format"""
+    return f"https://rest.kegg.jp/get/{user_input}/kgml"
+
+
+def pathway_text_url(user_input: str) -> str:
+    """Returns the URL for pathway information in plain text"""
+    return f"https://rest.kegg.jp/get/{user_input}"
+
+
+def get_organism_pathways(organism_code: str) -> str:
+    """Queries KEGG and returns a str of pathways"""
+    get_url = pathway_list_url(organism_code)
+    logger.info(f"Querying {organism_code} with {get_url}")
+    response = requests.get(get_url)
+    if response.status_code != 200:
+        logger.error(f"Request failed with status code {response.status_code}")
+        sys.exit()
+
+    return response.text
+
+
+def parse_organism_pathways(kegg_organism_pathways: str) -> list[str]:
+    """Parses the string return from KEGG into a list of pathways"""
+    paths = kegg_organism_pathways.split("\n")
+    paths = [path.split("\t")[0].removeprefix("path:") for path in paths if path]
+
+    return paths
+
+
+def get_pathway_kgml(pathway_code: str) -> str:
+    """Queries KEGG for a pathway and returns a tuple of path code and the pathway in KGML format"""
+    url = pathway_kgml_url(pathway_code)
+    response = requests.get(url)
+
+    return pathway_code, response.text
+
+
+async def send_async_kgml_request(pathway_code: str) -> str:
+    """Async wrapper for the KGML requester"""
+    return await asyncio.to_thread(get_pathway_kgml, pathway_code)
+
+
+def build_path_from_kgml(path_code: str, root: ET.ElementTree) -> list[str]:
+    """Parses teh KGML root (ElementTree object) and extracts the Compounds with C[0-9]{5} format"""
+    regex = re.compile("C[0-9]{5}")
+    compound_list = [
+        path_code,
+    ]
     for entry in root.findall("entry"):
         if entry.attrib["type"] == "compound":
-            tmpCpd = entry.attrib["name"]
-            tmpPath.extend(tmpCpd.split(" "))
-            tmpPath = [i for i in tmpPath if "cpd:" in i]
-            if tmpPath:
-                tmpPath = [re.sub("cpd:", "", i) for i in tmpPath]
-                tmpPathRet.extend(list(set(tmpPath)))
-            else:
-                return []
-    return tmpPathRet
+            compund_entry = entry.attrib["name"].removeprefix("cpd:")
+            results = regex.findall(compund_entry)
+            compound_list.extend(results)
+
+    return compound_list
 
 
-def combinePaths(MasterList, counter, Total):
-    # Sends API requests for KGML files per pathway, turns the compounds into a list and writes it in to a file,
-    # Takes MasterListm counter and Total all are created in main()
-    with open(sys.argv[2], "w") as fh:
-        for PathCode in MasterList:
-            print("Compiling:", PathCode)
-            APIres = getAPI(PathCode, "pathKGML")
-            checkAPI(APIres)
-            root = ET.fromstring(APIres.content)
-            tmpPath = buildPathKGML(root)
-            if tmpPath:
-                print(PathCode + "\t" + "\t".join(tmpPath), file=fh)
-            else:
-                print(PathCode, file=fh)
-            counter += 1
-            print(counter, "of", Total, "completed.")
-    print("Pathway table creted in:", sys.argv[2])
+def pad_list_items(input_list: list[list]) -> list[list]:
+    """Pads each list in a list of lists with "" upto list with most items"""
+    max_len = max([len(sublist) for sublist in input_list])
+    padded_list = []
+    for sub_list in input_list:
+        padding = [
+            "",
+        ] * (max_len - len(sub_list))
+        padded_list.append(sub_list + padding)
+
+    return padded_list
 
 
-def main():
-    # 1st block uses the first terminal input to create an API request for list of pathways
-    # 2nd block creates the master list to be used in table
-    # 3rd block sets the counters for the script and combines the pathways into a table
-    print("Retrieveing Pathway list for", sys.argv[1])
-    APIres = getAPI(sys.argv[1], "list")
-    checkAPI(APIres)
-    APIres = APIres.text.split("\n")[0:-1]
+def sort_lists(input_list: list[list]) -> list[list]:
+    "Sorts a list of lists based on the first element of each list"
+    return sorted(input_list, key=lambda x: x[0])
 
-    print("Creating master list for building table")
-    MasterList = []
-    for path in APIres:
-        tmpcode = path.split("\t")[0]
-        tmpcode = re.sub("path:", "", tmpcode)
-        MasterList.append(tmpcode)
-    print("Completed!\n")
 
-    counter = 0
-    Total = len(MasterList)
-    combinePaths(MasterList, counter, Total)
+def transpose_table(input_list: list[list]) -> list[tuple]:
+    """Transposes a list of lists with equal lengths"""
+    return list(zip(*input_list))
+
+
+async def main():
+    """Main async function"""
+    organism_code = "hsa"
+    paths = get_organism_pathways(organism_code)
+    paths = parse_organism_pathways(paths)
+    list_len = len(paths)
+
+    logger.info(f"Path List RECEIVED for {organism_code}: {list_len} Pathways.")
+    responses = await asyncio.gather(*[send_async_kgml_request(path) for path in paths])
+    kgml_roots = [
+        (path_code, ET.fromstring(response)) for path_code, response in responses
+    ]
+    result = [
+        build_path_from_kgml(path_code, kgml_root)
+        for path_code, kgml_root in kgml_roots
+    ]
+
+    result = sort_lists(result)
+    result = pad_list_items(result)
+    result = transpose_table(result)
+
+    output_str = ["\t".join(line) + "\n" for line in result]
+    with open("OUTPUT.tsv", "w") as fh:
+        fh.writelines(output_str)
 
 
 # Initiate Script
 if __name__ == "__main__":
-    sys.argv = ["qwe", "hsa", "old_OUT.tsv"]
-    start = perf_counter()
-    main()
-    print(f"Time: {perf_counter() - start}")
+    # start = perf_counter()
+    asyncio.run(main())
+    # print(f"Time = {perf_counter() - start}")
